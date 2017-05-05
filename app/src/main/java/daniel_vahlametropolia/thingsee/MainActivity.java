3c4ed8742e924eb097734589cd9669b979aa2bd6
Daniel Vahla <vahla.daniel@gmail.com>
2017-05-05 11:08:56 +0300

    Calculations added

--- app/src/main/java/daniel_vahlametropolia/thingsee/MainActivity.java
+++ app/src/main/java/daniel_vahlametropolia/thingsee/MainActivity.java
@@ -21,10 +21,12 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.database.CursorIndexOutOfBoundsException;
 import android.location.Location;
 import android.os.AsyncTask;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -41,14 +43,33 @@ import org.json.JSONArray;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Matcher;
 
 public class MainActivity extends Activity implements View.OnClickListener {
-    private static final int    MAXPOSITIONS = 20;
+    private static final int    MAXPOSITIONS = 1;
     private static final String PREFERENCEID = "Credentials";
 
     private String               username, password;
     private String[]             positions = new String[MAXPOSITIONS];
     private ArrayAdapter<String> myAdapter;
+
+    private Handler handler;
+    private int interval = 5000;
+
+    private Location FirstLoc = null;
+    private Location LastLoc = null;
+    private Location CurrentLoc = null;
+
+    private Double TotalDistance = null;
+    private Double CurrentSpeed = null;
+    private Double AverageSpeed = null;
+    private Double DeltaDist = null;
+
+    private Double TotalTime = null;
+    private Double CurrentTime = null;
+    private Double LastTime = null;
+    private Double FirstTime = null;
+    private Double DeltaTime = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +97,30 @@ public class MainActivity extends Activity implements View.OnClickListener {
         if (username.length() == 0 || password.length() == 0)
             // no, ask them from the user
             queryDialog(this, getResources().getString(R.string.prompt));
+
+        //create a handler for repeating tasks
+        handler = new Handler();
+    }
+    private Runnable UpdateData = new Runnable() {
+        @Override
+        public void run() {
+            // we make the request to the Thingsee cloud server in backgroud
+            // (AsyncTask) so that we don't block the UI (to prevent ANR state, Android Not Responding)
+            new TalkToThingsee().execute("QueryState");
+
+            //add delay for executing this task again
+            handler.postDelayed(this,interval);
+            }
+        };
+
+    // Call this method to start repeatedly requesting updates from Cloud
+    private void startRepeatingTask(){
+        UpdateData.run();
+    }
+
+    // Call this method to stop requesting updates from Cloud
+    private void stopRepeatingTask(){
+        handler.removeCallbacks(UpdateData);
     }
 
     private void queryDialog(Context context, String msg) {
@@ -130,9 +175,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         Log.d("USR", "Button pressed");
 
-        // we make the request to the Thingsee cloud server in backgroud
-        // (AsyncTask) so that we don't block the UI (to prevent ANR state, Android Not Responding)
-        new TalkToThingsee().execute("QueryState");
+        //Will start repeating task upon clicking button
+        // ***** Make one button to start and one button to stop ****
+        startRepeatingTask();
     }
 
     /* This class communicates with the ThingSee client on a separate thread (background processing)
@@ -174,14 +219,18 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 for (int i = 0; i < coordinates.size(); i++) {
                     Location loc = coordinates.get(i);
 
+                    CalculationRoutine(loc);
+
+                    /* Old line for showing stuff
                     positions[i] = (new Date(loc.getTime())) +
                                    " (" + loc.getLatitude() + "," +
                                    loc.getLongitude() + ")"; //coordinates.get(i).toString();
+                                   */
                 }
             } else {
                 // no, tell that to the user and ask a new username/password pair
-                positions[0] = getResources().getString(R.string.no_connection);
-                queryDialog(MainActivity.this, getResources().getString(R.string.info_prompt));
+                // *** now it will try again ***
+                new TalkToThingsee().execute("QueryState");
             }
             myAdapter.notifyDataSetChanged();
         }
@@ -197,4 +246,53 @@ public class MainActivity extends Activity implements View.OnClickListener {
         @Override
         protected void onProgressUpdate(Integer... values) {}
     }
+
+    private void CalculationRoutine(Location loc){
+        CurrentLoc = loc;
+        CurrentTime = (double)loc.getTime() / 1000;
+
+        if(FirstLoc == null){
+            FirstLoc = loc;
+            LastLoc = loc;
+            FirstTime = (double)loc.getTime() / 1000;
+            LastTime = (double)loc.getTime() / 1000;
+            CurrentSpeed = 0d;
+            AverageSpeed = 0d;
+            TotalDistance = 0d;
+            TotalTime = 0d;
+            DeltaTime = 0d;
+        }
+
+        if(CurrentLoc!=LastLoc){
+            TotalTime = CurrentTime - FirstTime;
+            DeltaTime = CurrentTime - LastTime;
+
+            DeltaDist = HaversineDistance(CurrentLoc,LastLoc);
+            TotalDistance += DeltaDist;
+
+            CurrentSpeed = DeltaDist / DeltaTime;
+            AverageSpeed = TotalDistance / TotalTime;
+
+            LastLoc = CurrentLoc;
+            LastTime = CurrentTime;
+        }
+
+    }
+    private double HaversineDistance(Location current, Location last){
+        double EARTH_RADIUS = 6371d;
+
+        double dLat = Math.toRadians(( current.getLatitude() - last.getLatitude() ));
+        double dLong = Math.toRadians((current.getLongitude() - last.getLongitude() ));
+
+        double startLat = Math.toRadians(last.getLatitude());
+        double endLat = Math.toRadians(current.getLatitude());
+
+        double a = Math.pow(Math.sin(dLat /2),2) +  Math.pow(Math.sin(dLong /2),2) * Math.cos(endLat) * Math.cos(startLat);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        double d = EARTH_RADIUS * c;
+
+        return d;
+    }
+
 }
